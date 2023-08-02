7fb48dfb6c5f770ac2cd937088e38fdd336b4a03
Cezar Gabriel <58271805+CezarGab@users.noreply.github.com>
2023-08-01 22:54:46 -0300

    Update url_string_parser.js

--- docs/assets/js/url_string_parser.js
+++ docs/assets/js/url_string_parser.js
@@ -1,13 +1,14 @@
-// Função para extrair o valor do parâmetro "code" da URL
-function getParameterValue(parameter) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(parameter);
-}
+document.addEventListener("DOMContentLoaded", function() {
+    // Função para extrair o valor do parâmetro "code" da URL
+    function getParameterValue(parameter) {
+        const urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(parameter);
+    }
 
-// Obter o valor do parâmetro "code" e exibi-lo na página (da autenticação por link do spotify)
-const codeValue = getParameterValue("code");
-
-document.getElementById("codeValue").value = codeValue;
+    // Obter o valor do parâmetro "code" e exibi-lo na página
+    const codeValue = getParameterValue("code");
+    document.getElementById("codeValue").value = codeValue || "Open Overlyrics and follow the instructions.";
+});
 
 // Função para o módulo copy-to-clipboard
 let copyText = document.querySelector(".copy-text");
